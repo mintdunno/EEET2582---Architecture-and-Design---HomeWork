5ad29bd9fc1ce795295fdc560d1025aef271da86
unknown <tnm1042002@gmail.com>
2024-11-05 16:16:42 +0700

    fdfdfdfd

--- week1/tutorial1/src/components/carousel/Carousel.jsx
+++ week1/tutorial1/src/components/carousel/Carousel.jsx
@@ -1,11 +1,12 @@
 import CarouselBody from "./child_component/carousel_body/CarouselBody";
+import getImagePaths from "./CarouselApiCall";
 
 const images = getImagePaths();
 
 const Carousel = () => {
     return (
         <div className="relative w-1/2 mx-auto h-[400px]">
-            
+            <CarouselBody imageUrl={images[0]} />
         </div>
     );
 };
