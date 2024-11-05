f38cfbb3d31b0ce12cfb339957691e910f47365b
MinT <tnm1042002@gmail.com>
2024-11-05 22:33:22 +0700

    automatic carousel

--- week1/tutorial1/src/components/carousel/Carousel.jsx
+++ week1/tutorial1/src/components/carousel/Carousel.jsx
@@ -1,13 +1,20 @@
+import React from "react";
 import CarouselBody from "./child_component/carousel_body/CarouselBody";
 import getImagePaths from "./CarouselApiCall";
+import useCarouselEventHandlers from "./CarousellEventHandler";
 
+// Fetch the image paths
 const images = getImagePaths();
 
 const Carousel = () => {
-    return (
-        <div className="relative w-1/2 mx-auto h-[400px]">
-            <CarouselBody imageUrl={images[0]} />
-        </div>
-    );
+  // Use the custom hook to get the current index
+  const { currentIndex } = useCarouselEventHandlers(images);
+
+  return (
+    <div className="relative w-1/2 mx-auto h-[400px]">
+      <CarouselBody imageUrl={images[currentIndex]} />
+    </div>
+  );
 };
+
 export default Carousel;
--- week1/tutorial1/src/components/carousel/CarousellEventHandler.js
+++ week1/tutorial1/src/components/carousel/CarousellEventHandler.js
@@ -1,3 +1,15 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
+const useCarouselEventHandlers = (images) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) =>
+                prevIndex === images.length - 1 ? 0 : prevIndex + 1
+            );
+        }, 2000); // Change slide every 2 seconds
+        return () => clearInterval(interval);
+    }, [images.length]);
+    return { currentIndex };
+}
+export default useCarouselEventHandlers;
